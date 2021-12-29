f17699ad059dd36b1ce03b56b96076d0e382dcf4
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2021-12-29 15:36:45 +0100

    Allow annotation on parameters

--- annotation/src/main/java/com/meetinclass/html/color/NamedColor.java
+++ annotation/src/main/java/com/meetinclass/html/color/NamedColor.java
@@ -1,5 +1,9 @@
 package com.meetinclass.html.color;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.PARAMETER})
 public @interface NamedColor {
     String value();
 }
