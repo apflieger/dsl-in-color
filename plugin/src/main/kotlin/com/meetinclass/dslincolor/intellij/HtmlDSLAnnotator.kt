5a6db045164e3dd16e68d2ed9437f6feb456f767
张志豪 <izhangzhihao@hotmail.com>
2022-01-04 09:14:13 +0800

    Refactor HtmlDSLAnnotator

--- plugin/src/main/kotlin/com/meetinclass/dslincolor/intellij/HtmlDSLAnnotator.kt
+++ plugin/src/main/kotlin/com/meetinclass/dslincolor/intellij/HtmlDSLAnnotator.kt
@@ -5,11 +5,13 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.editor.colors.TextAttributesScheme
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLiteralExpression
 import com.intellij.psi.PsiMethodCallExpression
-import com.meetinclass.dslincolor.intellij.ColorEx.getColorByAnnotationValue
+import java.awt.Color
 
 class HtmlDSLAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
@@ -41,27 +43,39 @@ class HtmlDSLAnnotator : Annotator {
         }
     }
 
-    private fun colorify(
-        holder: AnnotationHolder,
-        element: PsiMethodCallExpression,
-        annotationValue: String
-    ) {
-        val scheme = EditorColorsManager.getInstance().globalScheme
-        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-            .range(element.methodExpression)
-            .enforcedTextAttributes(
-                TextAttributes(
-                    scheme.getColorByAnnotationValue(annotationValue),
-                    null,
-                    null,
-                    null,
-                    0
-                )
-            )
-            .create()
-    }
-
     companion object {
         val NAMED_COLOR_ANNOTATION: String = com.meetinclass.dslincolor.annotations.NamedColor::class.java.name
+
+        private fun TextAttributesScheme.getColorByAnnotationValue(value: String): Color {
+            val stringValue = trimAnnotationValue(value)
+            val attributes = getAttributes(TextAttributesKey.createTextAttributesKey(stringValue))
+            return if (attributes == null || attributes.foregroundColor == null) {
+                Color.WHITE
+            } else {
+                attributes.foregroundColor
+            }
+        }
+
+        private fun trimAnnotationValue(value: String): String = value.trimStart('"').trimEnd('"')
+
+        private fun colorify(
+            holder: AnnotationHolder,
+            element: PsiMethodCallExpression,
+            annotationValue: String
+        ) {
+            val scheme = EditorColorsManager.getInstance().globalScheme
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(element.methodExpression)
+                .enforcedTextAttributes(
+                    TextAttributes(
+                        scheme.getColorByAnnotationValue(annotationValue),
+                        null,
+                        null,
+                        null,
+                        0
+                    )
+                )
+                .create()
+        }
     }
 }
